2e51493c47e8bedf9e1acafb638adf4d79eec362
Kunal Kene <kunalkene1797@gmail.com>
2017-08-06 19:23:56 +0530

    Update Icons For This Application

--- app/src/main/java/io/github/kunalkene1797/passwordmanager/LoginPage.java
+++ app/src/main/java/io/github/kunalkene1797/passwordmanager/LoginPage.java
@@ -2,6 +2,8 @@ package io.github.kunalkene1797.passwordmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -13,6 +15,22 @@ public class LoginPage extends AppCompatActivity {
     Button register, login;
     EditText username, password;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings Menu Activity Will Be Launched", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +54,7 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (username.length()==0 && password.length()==0) {
-                    Toast.makeText(getBaseContext(), "ID/Password Can't Be Blank", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "Username/Password Can't Be Blank", Toast.LENGTH_SHORT).show();
                 }
                 else if(username.length()==0){
                     Toast.makeText(getBaseContext(), "Username Can't Be Blank", Toast.LENGTH_SHORT).show();
@@ -49,5 +67,6 @@ public class LoginPage extends AppCompatActivity {
                 }
             }
         });
+
     }
 }
